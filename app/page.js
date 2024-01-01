ad96f374b309022e3663996d53fa7762e478fdb2
Miki Stec <mikulas@stec.sk>
2024-01-02 00:00:11 +0100

    Navbar added on mainpage

--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>Hello You!</div>
-  )
+    <nav>
+      <ul className='m-10 font-signika text-xl flex gap-2'> 
+        <li>
+          <Link href="/animate">animate</Link>
+        </li>
+        <li>
+          <Link href="/layout">layout</Link>
+        </li>
+        <li>
+          <Link href="/font">font</Link>
+        </li>
+        <li>
+          <Link href="/extend">extend</Link>
+        </li>
+        <li>
+          <Link href="/responsive">responsive</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
